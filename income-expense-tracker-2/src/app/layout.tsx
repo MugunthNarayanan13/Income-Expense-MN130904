300964fbc47b5a33843281a7a732d82522ed3bb9
Mugunth Narayanan <mugunthnarayanan@gmail.com>
2025-01-31 16:22:30 +0530

    ShadeCN setup, added button and sidebar

--- income-expense-tracker-2/src/app/layout.tsx
+++ income-expense-tracker-2/src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
       </body>
     </html>
   );
